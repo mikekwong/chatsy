dc55892f2239350d9c0ec408767ecb2ceeac239c
Mike Wong <wongmdesign@gmail.com>
2020-01-22 12:11:50 -0500

    store state passed and render sidebar

--- src/App.js
+++ src/App.js
@@ -2,40 +2,14 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
+import store from "./store";
+import _ from "lodash";
 
 const App = () => {
-  // const state = {
-  //   user: [],
-  //   messages: {
-  //     user_id: {
-  //       0: { text, is_user_msg },
-  //       1: { text, is_user_msg }
-  //     },
-  //     user_id_2: {
-  //       0: { text, is_user_msg }
-  //     }
-  //   },
-
-  //   contacts: {
-  //     user_id: {
-  //       name,
-  //       email,
-  //       profile_pic,
-  //       status,
-  //       user_id
-  //     },
-  //     user_id_2: {
-  //       name,
-  //       email,
-  //       profile_pic,
-  //       status,
-  //       user_id_2
-  //     }
-  //   }
-  // }
+  const { contacts } = store.getState();
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)} />
       <Main />
     </div>
   );
